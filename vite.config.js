4ddd94e6858f6dc802b20f86ac9352ab153875f1
John Doe <johndoe@example.com>
2024-02-07 11:12:38 +0100

    quitado path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, "src"),
-  server: {
-    port: 8080,
-    hot: true,
-  },
 });
